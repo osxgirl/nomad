c756f383407b5560a0ec47d651fd2a9f2efa697c
osxgirl <osxgirl@mac.com>
2021-08-31 23:47:40 -0400

    index erb

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+    root 'welcome#index'
     namespace :api do
       namespace :v1 do
         resources :coaches, only: [:index, :create, :destroy, :update]
-        
-        get 'coaches', to: 'coaches#index'
+        root 'welcome#index'
       end
     end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
