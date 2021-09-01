e540e87a2dd753a4dd99872318e079544af5768f
osxgirl <osxgirl@mac.com>
2021-08-31 21:57:24 -0400

    Appointment Component

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
         resources :coaches, only: [:index, :create, :destroy, :update]
+        resources :welcome
         
-        get 'coaches', to: 'coaches#index'
+        root to: 'welcome#index'
       end
     end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
